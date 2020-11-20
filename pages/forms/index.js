9b9bd374879a1d926a108ea3b5812ab2e782786e
Hope Tambala <hopetambala@outlook.com>
2020-11-20 00:30:54 -0500

    feat: create navbar

--- pages/forms/index.js
+++ pages/forms/index.js
@@ -1,58 +1,16 @@
-import Head from 'next/head';
-import Card from 'components/Card';
+import Layout from 'components/Layout';
 import Tabley from 'components/Tabley';
 
 export default function Home() {
   return (
+    <Layout>
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main>
         <h1 className="title">
-          Welcome to
+          I'm a Table
           {' '}
           <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className="description">
-          Get started by editing
-          {' '}
-          <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <Card title="Documentation" description="Find in-depth information about Next.js features and API." />
-          <div href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </div>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
         <Tabley />
       </main>
 
@@ -201,22 +159,7 @@ export default function Home() {
       `}
       </style>
 
-      <style jsx global>
-        {`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}
-      </style>
     </div>
+    </Layout>
   );
 }
