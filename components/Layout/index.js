18b19ed2a977e2432e4dc255222aeae508805188
Hope Tambala <hopetambala@outlook.com>
2021-01-03 13:41:02 -0500

    fix: add MUI to next.js app

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -8,6 +8,7 @@ import { fade, makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    margin:0
   },
   menuButton: {
     marginRight: theme.spacing(2),
--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,37 @@
-import NextApp from 'next/app'
-import React from 'react'
-import { ThemeProvider } from 'styled-components'
-const theme = {
-  primary: 'green',
-}
-export default class App extends NextApp {
-  // remove it here
-  componentDidMount() {
-    const jssStyles = document.querySelector('#jss-server-side')
-    if (jssStyles && jssStyles.parentNode)
-      jssStyles.parentNode.removeChild(jssStyles)
-  }
-  render() {
-    const { Component, pageProps } = this.props
-    return (
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from 'modules/theme';
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
       <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
-    )
-  }
+    </React.Fragment>
+  );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
