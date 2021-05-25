ae02a6cc6e5ce8b839188f88d25a908bc0334827
Joseph McCombs <jamccomb92@gmail.com>
2021-05-24 21:10:07 -0400

    fix: message to show user multiple geolocation multiple times and updated id

--- app/components/FormCreator/FormTemplate/InputPicker/Geolocation/index.js
+++ app/components/FormCreator/FormTemplate/InputPicker/Geolocation/index.js
@@ -1,5 +1,5 @@
 import { Button } from '@material-ui/core';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from './index.module.scss';
 
@@ -10,17 +10,22 @@ const Input = (props) => {
     removeValue,
   } = props;
 
+  const [geolocationCount, setGeolocationCount] = useState(0);
+  
   useEffect(() => {
-
     const elementsIndex = formItems.findIndex((element) => element.id === item.id);
     const newArray = [...formItems];
     newArray[elementsIndex] = {
       ...newArray[elementsIndex],
-      formikKey: 'location_'+item.id,
+      formikKey: 'geolocation_'+item.id.slice(0,4),
     };
-
     setFormItems(newArray);
   }, [])
+
+  useEffect(() => {
+    const geolocationFields = formItems.filter((element) => element.fieldType === 'geolocation');
+    setGeolocationCount(geolocationFields.length)
+  }, [formItems])
 
   return (
     <div className={styles.element}>
@@ -28,6 +33,9 @@ const Input = (props) => {
         <div key={item.id}>
           <h3>Geolocation</h3>
           <Button variant="contained" className={styles.remove} onClick={() => removeValue(item.id)}>Remove Question</Button>
+          {geolocationCount > 1 && (
+            <h5>There should be only one geolocation field per form. Remove one before submission.</h5>
+          )}
         </div>
       )}
     </div>
